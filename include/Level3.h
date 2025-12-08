2253e4d2b47550898787906ebbab5522faee1963
TheRealMrNumbers <cameron.barrett@richmond.edu>
2025-12-08 13:55:10 -0500

    docstrings updated for the rest of the headers

--- include/Level3.h
+++ include/Level3.h
@@ -6,11 +6,33 @@
 class Level3 : public LevelBase
 {
 public:
+    /**
+     * Initializes an instance of level 3
+     * 
+     * @param parent the parent window, since Level 3 consists of a few specific frames out of the greater app
+     */
     Level3(wxWindow* parent);
+
+    /**
+     * Loads the specific screen from the level's preset asset library via indexing
+     * 
+     * @param screen the indexed screen to be loaded
+     */
     void LoadScreen(int screen) override;
 
 private:
+    /**
+     * Simulates a wifi outage by removing the player's ability to interact with the page for a randomized period of time.
+     * 
+     * @param onFinished callback void function needed to complete the process
+     */
     void SimulateWifiOutage(std::function<void()> onFinished);
+
+    /**
+     * Simulates a page-loading scenario by switching to a loading asset for a randomized period of time.
+     * 
+     * @param onFinished callback void function needed to complete the process
+     */
     void SimulateLoadingOnly(std::function<void()> onFinished);
 
     bool m_isLoading = false;
--- include/ResultsScreen.h
+++ include/ResultsScreen.h
@@ -7,6 +7,14 @@ class ResultsScreen : public wxPanel
 {
 public:
     std::function<void()> onBack;
+    /**
+     * Initializes the results screen with the data taken from the player's playtime, the landing page, and the leaderboard
+     * 
+     * @param parent the parent window from which this particular window is an offshoot in the app
+     * @param elapsedMs the elapsed time, in milliseconds, that it took the player to complete the game
+     * @param playerName the player's name, as read by the leaderboard
+     * @param levelIndex the level of the game that the player completed, as read by the landing page
+     */
     ResultsScreen(wxWindow* parent, long elapsedMs, const wxString& playerName, int levelIndex);
 
 private:
